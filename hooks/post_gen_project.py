171da8a294446409e977119068ab56f6688b3ec6
Clément GUICHARD <clement.guichard0@gmail.com>
2025-03-18 22:05:59 +0100

    refactor: compute variables in cookiecutter.json for git repo path and origin

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,7 @@ import subprocess
 
 # Git init
 subprocess.call(["git", "init", "--initial-branch", "{{ cookiecutter.git_branch }}"])
-subprocess.call(["git", "remote", "add", "origin", "git@github.com:{{ cookiecutter.git_user }}/{{ cookiecutter.project_slug }}.git"])
+subprocess.call(["git", "remote", "add", "origin", "{{ cookiecutter.__git_repo_origin }}"])
 subprocess.call(["git", "add", "."])
 subprocess.call(["git", "commit", "-m", "chore: initial commit"])
 {% endif %}
