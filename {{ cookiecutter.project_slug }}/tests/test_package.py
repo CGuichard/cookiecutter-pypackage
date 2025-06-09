86e6f1f28d4573cf841d0957799ba6837b9144db
Clément GUICHARD <clement.guichard0@gmail.com>
2025-06-09 17:45:29 +0200

    fix(test): add annotations

--- {{ cookiecutter.project_slug }}/tests/test_package.py
+++ {{ cookiecutter.project_slug }}/tests/test_package.py
@@ -1,12 +1,12 @@
 """Basic package test."""
 
 
-def test_package_import():
+def test_package_import() -> None:
     """Import package."""
     import {{ cookiecutter.pkg_name }}  # noqa: F401
 
 
-def test_package_version_is_defined():
+def test_package_version_is_defined() -> None:
     """Check imported package have __version__ defined."""
     import {{ cookiecutter.pkg_name }}
 
