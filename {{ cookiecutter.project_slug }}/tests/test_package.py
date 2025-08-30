507c89722828eeedd771f5e0df0fd62ce039a5f1
Clément GUICHARD <clement.guichard0@gmail.com>
2025-08-30 17:57:05 +0200

    feat: update dev dependencies

--- {{ cookiecutter.project_slug }}/tests/test_package.py
+++ {{ cookiecutter.project_slug }}/tests/test_package.py
@@ -3,11 +3,11 @@
 
 def test_package_import() -> None:
     """Import package."""
-    import {{ cookiecutter.pkg_name }}  # noqa: F401
+    import {{ cookiecutter.pkg_name }}  # noqa: F401, PLC0415
 
 
 def test_package_version_is_defined() -> None:
     """Check imported package have __version__ defined."""
-    import {{ cookiecutter.pkg_name }}
+    import {{ cookiecutter.pkg_name }}  # noqa: PLC0415
 
     assert {{ cookiecutter.pkg_name }}.__version__ != "undefined"
